9fa61790fe69d39e729b9cb997eeaf2e3ea654c9
AnaLuizaElert <ana_elert@estudante.sesisenai.org.br>
2023-05-07 15:44:44 -0300

    add info from local storage

--- pontuacao.js
+++ pontuacao.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() { 
-    document.getElementById("pontuationEasy").innerHTML = localStorage.getItem("pontuacaoEasy");
-    document.getElementById("pontuationMedium").innerHTML = localStorage.getItem("pontuacaoMedium");
-    document.getElementById("pontuationHard").innerHTML = localStorage.getItem("pontuacaoHard");
-    document.getElementById("pontuationImpossible").innerHTML = localStorage.getItem("pontuacaoImp");
+    document.getElementById("pontuationEasy").innerHTML = localStorage.getItem("pontuationEasy");
+    document.getElementById("pontuationMedium").innerHTML = localStorage.getItem("pontuationMedium");
+    document.getElementById("pontuationHard").innerHTML = localStorage.getItem("pontuationHard");
+    document.getElementById("pontuationImpossible").innerHTML = localStorage.getItem("pontuationImpossible");
 })
